ab881117d3dd4e07136397a880ee717a6dba3d9c
Hicham <121328301+hcmbn7@users.noreply.github.com>
2025-04-27 12:23:09 -0400

    Update questions

--- world-cup-bd/src/App.js
+++ world-cup-bd/src/App.js
@@ -6,19 +6,19 @@ const API_URL = process.env.REACT_APP_API_URL;
 const questions = [
   {
     id: 1,
-    text: "Top 10 pays ayant gagné le plus de Coupes du Monde",
+    text: "Quels sont les 10 pays ayant remporté le plus de finales, en distinguant les victoires masculines et féminines ?",
   },
   {
     id: 2,
-    text: "Joueuses ayant reçu le plus de cartons, détail jaune/rouge",
+    text: "Quels sont les 20 joueurs masculins ayant reçu le plus de cartons, en précisant combien sont jaunes et combien sont rouges ?",
   },
   {
     id: 3,
-    text: "Sélectionneurs ayant déjà été joueurs en Coupe du Monde",
+    text: " Pour chaque personne ayant d’abord joué en équipe nationale puis exercé comme sélectionneur, indiquer (1) les années où elle a joué ; (2) le pays qu’elle représentait comme joueur ; (3) les années où elle a été sélectionneur ; (4) le pays qu’elle a entraîné.",
   },
   {
     id: 4,
-    text: "Personnes ayant remporté le plus de Coupes (joueur + coach)",
+    text: "Quelles sont les 25 personnes qui ont le plus grand nombre de coupes remportées (totales joueur + sélectionneur) et, pour chacune, combien de finales elles ont disputées au total ?",
   },
 ];
 
